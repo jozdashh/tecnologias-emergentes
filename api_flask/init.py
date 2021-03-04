cb05c72fcee210d5faefd92c21e8d96ee05f4116
Josue Peña <jozdashh@javerianacali.edu.co>
2021-03-04 09:27:54 -0500

    Update init.py

--- api_flask/init.py
+++ api_flask/init.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 # Inits and configs
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@10.188.64.2/flask'
 app.secret_key = '#0#'
